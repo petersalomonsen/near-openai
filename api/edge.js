af72f7027e64fb8ddaec80285e5283dcaa5d45f6
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-03-12 20:06:19 +0100

    require successful near transaction

--- api/edge.js
+++ api/edge.js
@@ -6,6 +6,21 @@ export const config = {
 
 export default async (request) => {
     const input = JSON.parse(new TextDecoder().decode((await request.body.getReader().read()).value));
+
+    const transactionResult = await fetch('https://rpc.mainnet.near.org', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                "jsonrpc": "2.0",
+                "id": "dontcare",
+                "method": "tx",
+                "params": [input.transaction_hash, input.sender_account_id]
+            }
+        )
+    });
     const openairesponse = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
@@ -18,11 +33,16 @@ export default async (request) => {
         })
     }).then(r => r.json());
 
-    return new Response(JSON.stringify(openairesponse), {
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST",
-            "Content-Type": "application/json"
-        }
-    });
+    if (transactionResult.result.status.SuccessValue!=undefined &&
+        transactionResult.result.transaction.received_id == 'jsinrust.near' &&
+        transactionResult.result.deposit > 10000_0000000000_0000000000n
+    ) {
+        return new Response(JSON.stringify(openairesponse), {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST",
+                "Content-Type": "application/json"
+            }
+        });
+    }
 };
