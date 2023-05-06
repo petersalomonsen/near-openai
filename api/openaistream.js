41e0a20d8509d15ce3464de6b38c2b5075f33113
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-05-06 16:31:49 +0200

    test readable stream

---
+++ api/openaistream.js
@@ -0,0 +1,19 @@
+const token = process.env.OPENAI_TOKEN;
+
+export const config = {
+    runtime: 'edge',
+};
+
+export default async (request) => {
+    const input = await request.json();
+
+    return new ReadableStream({
+        async start(controller) {
+            for(let n=0;n<60;n++) {
+                controller.enqueue('counting '+n+'\n');
+                await new Promise(r => setTimeout(() => r(), 1000));
+            }
+            controller.close();
+        }
+    });
+};
