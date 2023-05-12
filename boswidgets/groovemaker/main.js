c7f8cc8589f37746ce869bc1f20ecca7a4b5ac15
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-05-12 20:52:48 +0200

    groovemaker: download wav
    button to download the generated music as a wav file

--- boswidgets/groovemaker/main.js
+++ boswidgets/groovemaker/main.js
@@ -221,6 +221,61 @@ async function rendermusic(patterns) {
         audioCtx = new AudioContext();
         startAudioBufSrcNode();
     };
+
+    const leftBufferFloat = new Float32Array(leftbuffer);
+    const rightBufferFloat = new Float32Array(rightbuffer);
+
+    const bitDepth = 32;
+    const numChannels = 2;
+
+    const bytesPerSample = bitDepth / 8
+    const blockAlign = numChannels * bytesPerSample
+    const chunklength = bytesPerSample * 2 * leftBufferFloat.length;
+
+    const buffer = new ArrayBuffer(44 + chunklength);
+    const view = new DataView(buffer);
+    const writeString = (view, offset, string) => {
+        for (let i = 0; i < string.length; i++) {
+            view.setUint8(offset + i, string.charCodeAt(i))
+        }
+    }
+    /* RIFF identifier */
+    writeString(view, 0, 'RIFF')
+    /* RIFF chunk length */
+    view.setUint32(4, 36 + chunklength, true)
+    /* RIFF type */
+    writeString(view, 8, 'WAVE')
+    /* format chunk identifier */
+    writeString(view, 12, 'fmt ')
+    /* format chunk length */
+    view.setUint32(16, 16, true)
+    /* sample format (raw) */
+    view.setUint16(20, 3, true)
+    /* channel count */
+    view.setUint16(22, numChannels, true)
+    /* sample rate */
+    view.setUint32(24, sampleRate, true)
+    /* byte rate (sample rate * block align) */
+    view.setUint32(28, sampleRate * blockAlign, true)
+    /* block align (channel count * bytes per sample) */
+    view.setUint16(32, blockAlign, true)
+    /* bits per sample */
+    view.setUint16(34, bitDepth, true)
+    /* data chunk identifier */
+    writeString(view, 36, 'data')
+    /* data chunk length */
+    view.setUint32(40, chunklength, true)
+
+    let offset = 44;
+
+    for (let n = 0; n < leftBufferFloat.length; n++) {
+        view.setFloat32(offset, leftBufferFloat[n], true);
+        offset += 4;
+        view.setFloat32(offset, rightBufferFloat[n], true);
+        offset += 4;
+    }
+
+    window.parent.postMessage({ command: 'download', buffer }, globalThis.parentOrigin, [buffer]);
 }
 
 
